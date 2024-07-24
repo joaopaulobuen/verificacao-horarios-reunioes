8d972b254abe6e78ab9e789b2a92f1df52b7d507
joaopaulobuen <joaopaulobuen@gmail.com>
2024-07-24 14:18:49 -0300

    Commit inicial

---
+++ src/main/java/org/example/MeetingScheduler.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+// Classe TimeFactory:
+class TimeFactory {
+    // TODO: Crie o método estático para criar um objeto LocalTime a partir de uma string:
+    public static LocalTime createTime(String timeInput){
+        return LocalTime.parse(timeInput);
+    }
+
+}
+
+public class MeetingScheduler {
+
+    private static final LocalTime MIN_TIME = LocalTime.of(9, 0);
+    private static final LocalTime MAX_TIME = LocalTime.of(18, 0);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String desiredTimeInput = scanner.nextLine();
+        //TODO: Implemente o TimeFactory.createTime() para converter desiredTimeInput em LocalTime:
+        LocalTime desiredTime = TimeFactory.createTime(desiredTimeInput);
+
+
+
+        String currentTimeInput = scanner.nextLine();
+        //TODO: Implemente o TimeFactory.createTime() para converter currentTimeInput em LocalTime:
+        LocalTime currentTime = TimeFactory.createTime(currentTimeInput);
+
+        if (isMeetingSchedulable(desiredTime)) {
+            System.out.println("Reuniao pode ser agendada.");
+        } else {
+            System.out.println("Reuniao nao pode ser agendada. Horario fora do intervalo permitido.");
+        }
+    }
+
+    private static boolean isMeetingSchedulable(LocalTime desiredTime) {
+        return !desiredTime.isBefore(MIN_TIME) && !desiredTime.isAfter(MAX_TIME);
+    }
+}
